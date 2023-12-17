aed107fb4a5433bd9e911d47f34da96c9b8f6c0b
GABETROLL <gabriel7icplhappy@gmail.com>
2023-12-16 17:00:10 -0800

    Lint

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -3,9 +3,10 @@ import dbClient from '../utils/db';
 
 export default class AppController {
   static getStatus(request, response) {
-    response.send({ 'redis': redisClient.isAlive(), 'db': dbClient.isAlive() });
+    response.send({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
   }
+
   static async getStats(request, response) {
-    response.send({ 'users': await dbClient.nbUsers(), 'files': await dbClient.nbFiles() });
+    response.send({ users: await dbClient.nbUsers(), files: await dbClient.nbFiles() });
   }
 }
